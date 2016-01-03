c4a02ed01f4bafeb15a79fd16e61a93cafb37a50
Phil Cannata <phil.cannata@oracle.com>
2016-01-03 14:04:38 -0600

    Added temporary query to OracleRDFNoSQLInterface

--- jyhton/src/org/python/ReL/OracleRDFNoSQLInterface.java
+++ jyhton/src/org/python/ReL/OracleRDFNoSQLInterface.java
@@ -1,11 +1,13 @@
 package org.python.ReL;
 
-import java.sql.ResultSet;
-
 import com.hp.hpl.jena.graph.*;
 import com.hp.hpl.jena.query.*;
 import com.hp.hpl.jena.rdf.model.Model;
 import oracle.rdf.kv.client.jena.*;
+import com.hp.hpl.jena.sparql.core.*;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * A class that is used to interface with the oracle database.
@@ -46,6 +48,37 @@ public class OracleRDFNoSQLInterface extends DatabaseInterface {
     public void OracleNoSQLAddQuad(String graph, String subject, String predicate, String object)
     {
         System.out.println("In addQuad, stmt is: " + graph + ", " + subject + ", " + predicate + ", " + object);
-        datasetGraph.add(Node.createURI(graph), Node.createURI(subject), Node.createURI(predicate), Node.createURI(object));
+        datasetGraph.add(Node.createURI("carnot:" + graph), Node.createURI("carnot:" + subject), Node.createURI("carnot:" + predicate), Node.createURI("carnot:" + object));
+
+        Dataset ds = DatasetImpl.wrap(datasetGraph);
+
+        String szQuery = " PREFIX c: <carnot:> "                    + 
+                                                          // See URI discussion at https://en.wikipedia.org/wiki/Uniform_Resource_Identifier
+                                                          // For known non generic URI see https://gist.github.com/knu/744715/0143487cc3f3fffb0b6faf9b18b0da6f0642e7d5
+               " SELECT ?g ?s ?p ?o"           +
+               " WHERE "                       +
+               " { "                           +
+               "     GRAPH ?g { ?s ?p ?o }"    +
+               " } ";
+
+        Query query = QueryFactory.create(szQuery);
+        QueryExecution qexec = QueryExecutionFactory.create(query, ds);
+
+        try {
+            com.hp.hpl.jena.query.ResultSet queryResults = qexec.execSelect();
+                System.out.println("Made it to here 1.");
+                if(queryResults != null) {
+                    System.out.println("Made it to here 2. " + queryResults.toString());
+                    //ResultSetFormatter.out(System.out, queryResults, query);
+                    String xmlstr = "Initial string";
+                    xmlstr = ResultSetFormatter.asXMLString(queryResults); // For documentation, see http://grepcode.com/file/repo1.maven.org/maven2/com.hp.hpl.  
+                    System.out.println("xmlstr is: " + xmlstr);
+                    System.out.println("Made it to here 3.");
+                }
+            }
+        finally {
+          qexec.close();
+        }
+        //ds.close();
     }
 }
