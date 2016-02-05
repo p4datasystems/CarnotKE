11897a5690af8073d08e658528148309d5bd4428
CamelBackNotation <joshuahurt@utexas.edu>
2016-02-05 17:29:29 -0600

    Included necessary setup script for the Oracle NoSQL DB.

--- jyhton/sim_native_test.sh
+++ jyhton/sim_native_test.sh
@@ -3,4 +3,4 @@ ant clean
 rm -rf dist/
 ant
 ant
-dist/bin/jython -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 nativeSIMunitTests.py
+dist/bin/jython -J-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 nativeSIMunitTests.py
