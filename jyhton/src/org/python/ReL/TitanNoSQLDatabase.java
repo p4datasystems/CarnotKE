546b04c3980a2cc9d610a99b8117d7fed9d86289
Alvin Deng <alvin.q.deng@utexas.edu>
2016-03-01 15:05:04 -0600

    Update: Added some basic constructor of TitanNoSQL

--- jyhton/src/org/python/ReL/TitanNoSQLDatabase.java
+++ jyhton/src/org/python/ReL/TitanNoSQLDatabase.java
@@ -1,10 +1,12 @@
 package org.python.ReL;
+import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversal;
 import wdb.metadata.Adapter;
 import wdb.metadata.ClassDef;
 import wdb.metadata.IndexDef;
 import wdb.metadata.WDBObject;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import com.thinkaurelius.titan.core.*;
 import com.thinkaurelius.titan.core.attribute.Geoshape;
@@ -17,6 +19,7 @@ import org.apache.tinkerpop.gremlin.structure.Direction;
 import org.apache.tinkerpop.gremlin.structure.Edge;
 import org.apache.tinkerpop.gremlin.structure.T;
 import org.apache.tinkerpop.gremlin.structure.Vertex;
+import wdb.parser.QueryParser;
 
 /**
  * @author Alvin Deng
@@ -24,7 +27,53 @@ import org.apache.tinkerpop.gremlin.structure.Vertex;
  */
 public class TitanNoSQLDatabase extends DatabaseInterface {
 
+    public static QueryParser queryParser = null;
+    public static TitanGraph titanGraph = null;
+    public static TitanTransaction titanTransaction = null;
+    public static Object rootID = null;
+
+    public TitanNoSQLDatabase() {
+
+        this.adapter = new TitanNoSQLAdapter(this);
+
+        TitanFactory.Builder config = TitanFactory.build();
+        config.set("storage.backend", "cassandra");
+        config.set("storage.directory", "127.0.0.1");
+        titanGraph = config.open();
+
+        TitanManagement mg = titanGraph.openManagement();
+        boolean initGraph = mg.getGraphIndex("byClassDef") == null;
+
+        if (initGraph) {
+            VertexLabel classLabel = mg.makeVertexLabel("classDef").make();
+            PropertyKey name = mg.makePropertyKey("name").dataType(String.class).make();
+            mg.buildIndex("byClassDef", Vertex.class).addKey(name).indexOnly(classLabel).buildCompositeIndex();
+        }
+        mg.commit();
+
+        titanTransaction = titanGraph.newTransaction();
+        if (initGraph) {
+            Vertex root = titanTransaction.addVertex(T.label, "classDef", "name", "Root");
+
+            titanTransaction.commit();  // Regular classDefs can't have spaces
+            rootID = root.id();
+        } else {
+            GraphTraversalSource g = titanGraph.traversal();
+            g.V().hasLabel("classDef").forEachRemaining(n -> {
+                if (n.property("name").value().equals("Root")) {
+                    rootID = n.id();
+                }
+            });
+        }
+    }
+    
     private class TitanNoSQLAdapter implements Adapter {
+
+        private TitanNoSQLDatabase db;
+
+        private TitanNoSQLAdapter(TitanNoSQLDatabase db) {
+            this.db = db;
+        }
 
         @Override
         public void putClass(ClassDef classDef) {
@@ -36,11 +85,13 @@ public class TitanNoSQLDatabase extends DatabaseInterface {
             return null;
         }
 
+        /* InsertQuery */
         @Override
         public void putObject(WDBObject wdbObject) {
 
         }
 
+        /* RetrieveQuery */
         @Override
         public WDBObject getObject(String s, Integer integer) {
             return null;
@@ -48,7 +99,7 @@ public class TitanNoSQLDatabase extends DatabaseInterface {
 
         @Override
         public ArrayList<WDBObject> getObjects(IndexDef indexDef, String s) {
-            return null;
+            throw new UnsupportedOperationException();
         }
 
         @Override
@@ -60,5 +111,6 @@ public class TitanNoSQLDatabase extends DatabaseInterface {
         public void abort() {
 
         }
+
     }
 }
